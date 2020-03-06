decb92b27ef099fef8e1c0d3fd52697c0bfe32b5
Sydnej123 <songoqu12@gmail.com>
2020-03-06 20:32:10 +0100

    Initialize

---
+++ Form.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormComponent
+{
+    
+    public partial class Form : UserControl
+    {
+        private Dictionary<string, Component> componentMap;
+        private Dictionary<string, string> errorMap;
+        private Dictionary<string, string> regexMap;
+        
+        public Form()
+        {
+            InitializeComponent();
+            componentMap = new Dictionary<string, Component>();
+            errorMap = new Dictionary<string, string>();
+            regexMap = new Dictionary<string, string>();
+
+        }
+
+        public void addField(String name)
+        {
+
+        }
+
+        public void addError(String fieldName, String errorContent)
+        {
+
+        }
+
+        public void addRegex(String fieldName, String regex)
+        {
+
+        }
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormComponent
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Form form = new Form();
+            form.Width = 500;
+            this.Controls.Add(form);
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
